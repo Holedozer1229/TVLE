25766e846b9aa1b10b9d1b14910d4fb9d3ed5971
Holedozer1229 <holedozer@icloud.com>
2025-05-04 00:47:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
----
-
-### 3. `setup.py`
 ```python
 from setuptools import setup, find_packages
 
@@ -10,21 +7,21 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        "numpy>=1.23.0",
-        "matplotlib>=3.5.0",
-        "scipy>=1.8.0",
-        "tqdm>=4.64.0",
-        "ecdsa>=0.18.0",
+        "numpy>=1.26.4",
+        "matplotlib>=3.9.2",
+        "scipy>=1.14.1",
+        "tqdm>=4.66.5",
+        "ecdsa>=0.19.0",
         "base58>=2.1.1",
     ],
     author="Travis Jones",
-    author_email="travis.jones@example.com",  # Replace with actual email
+    author_email="holedozer@icloud.com",  # Replace with actual email
     description="Temporal Vector Lattice Entanglement Simulation",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/TravisJones/TVLE",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
     ],
 )
